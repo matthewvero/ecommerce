375c0ada8a2fec9f77a9fb0381cb75f9a2493c7b
matthewvero <mattavero@hotmail.com>
2020-02-20 16:08:06 +0000

    homepage broken in to components

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import './menu-item.styles.scss'
 
-const MenuItem = props => (
-    <div className='menu-item'>
+const MenuItem = ({id, title, imageUrl, size}) => (
+    <div key={id} className={`menu-item ${size}`}>
+        <div className={'background'} style={{
+            backgroundImage: `url(${imageUrl})`
+        }}></div>
         <div className='content'>
-            <h1 className='title'>{props.title}</h1>
+            <h1 className='title'>{title}</h1>
             <span className='sub-title'>Shop Now</span>
         </div>
     </div>
